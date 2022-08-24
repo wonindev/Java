8cf4580b93f89241f7ca76aa2706bbeb8992bc5f
wonindev <sangwonparkdev@gmail.com>
2022-08-25 02:44:20 +0900

    뺄셈구현

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,23 @@
 public class Calculator {
     public int run(String[] args){
+        int sum = 0;
         int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        return a+b;
+        int b = Integer.parseInt(args[2]);
+
+        switch (args[1]){
+            case "+":
+                sum = a + b;
+                break;
+
+            case "-":
+                sum = a - b;
+                break;
+
+            default:
+                break;
+        }
+        return sum;
+
     }
 
 }
