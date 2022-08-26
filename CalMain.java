12632447f5bd83f5921b38f3491d0363f6f3f39c
wonindev <sangwonparkdev@gmail.com>
2022-08-26 22:23:37 +0900

    문자열길이로 계산을 하니 오류가 생겨서 parsing하는 방법으로 변경

--- CalMain.java
+++ CalMain.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
+
 public class CalMain {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
 
-
         int num = 0;
-        num = cal.run(args);
 
-        if (num >= 0) {
-            System.out.println("출력: " + num);
+
+        if (args.length == 0) {
+            Scanner sc = new Scanner(System.in);
         }
-        
+
+        if (args.length >= 1) {
+            num = cal.run(args);
+            System.out.print("입력: ");
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+            }
+            System.out.println();
+
+            if (num >= 0) {
+                System.out.println("출력커스텀1: " + num);
+                System.out.println("출력커스텀2: 정답은 " + num + "입니다");
+                System.out.print("출력커스텀3: ");
+                for (int i = 0; i < args.length; i++) {
+                    System.out.print(args[i]);
+                }
+                System.out.print(" 의 정답은 " + num + "입니다");
+            }
+
+        }
+
+
     }
 }
--- Calculator.java
+++ Calculator.java
@@ -1,57 +1,23 @@
 public class Calculator {
     public int run(String[] args) {
         int sum = 0;
-        int cnt = 1;
 
-        if (args.length > 1) {
-            sum = Integer.parseInt(args[0]);
-            for (int i = 0; i < args.length; i += 2) {
-                if (args[cnt].equals("+")) {
-                    if (i == args.length - 1) {
-                        sum += Integer.parseInt(args[i]);
-                    } else {
-                        sum += Integer.parseInt(args[i + 2]);
-                    }
+        String temp[] = args[0].split("[+-]");
+        String oper[] = args[0].split("[0-9]");
 
-                } else if (args[cnt].equals("-")) {
-                    if (i == args.length - 1) {
-                        sum -= Integer.parseInt(args[i]);
-                    } else {
-                        sum -= Integer.parseInt(args[i + 2]);
-                    }
-                }
-                if (cnt == args.length - 2) break;
-                cnt += 2;
+        int num = Integer.parseInt(temp[0]);
 
+        for (int i = 1; i < oper.length; i++) {
+            if (oper[i].equals("+")) {
+                num += Integer.parseInt(temp[i]);
+            }
+
+            else if (oper[i].equals("-")) {
+                num -= Integer.parseInt(temp[i]);
             }
         }
 
-        else if (args.length == 1) {
-            String arr[] = args[0].split("");
-            sum = Integer.parseInt(arr[0]);
-
-            for (int i = 0; i < arr.length; i += 2) {
-                if (arr[cnt].equals("+")) {
-                    if (i == arr.length - 1) {
-                        sum += Integer.parseInt(arr[i]);
-                    } else {
-                        sum += Integer.parseInt(arr[i + 2]);
-                    }
-
-                } else if (arr[cnt].equals("-")) {
-                    if (i == arr.length - 1) {
-                        sum -= Integer.parseInt(arr[i]);
-                    } else {
-                        sum -= Integer.parseInt(arr[i + 2]);
-                    }
-                }
-                if (cnt == arr.length - 2) break;
-                cnt += 2;
-            }
-
-        }
-
-        return sum;
+        return num;
 
 
     }
