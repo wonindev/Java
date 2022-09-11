1844d2f307f2b62032b850b4de31ea32de9642d1
wonindev <sangwonparkdev@gmail.com>
2022-09-11 21:49:10 +0900

    클래스 작성완료 후 인터페이스 구현 전

--- 실습문제/자동차 정보 출력 실습/Driving.java
+++ 실습문제/자동차 정보 출력 실습/Driving.java
@@ -1,18 +1,18 @@
 
 interface Driving {
-	String TWO_WHEEL_DRIVE =  "이륜구동";
-	String FOUR_WHEEL_DRIVE =  "사륜구동";
+	String drivemode(String drive);
+	String gearmode(String gear);
 
 }
 
 interface PowerSource { // 자동차의 연료 표현. 가솔린, 디젤, 전기 등
-	String fueltype();
+	String fueltype(String brand);
 }
 
-interface MovingPower { // 자동차 구동력을 표현. 엔진 또는 전기모터 등.
-	String drivingpower();
+interface MovingPower extends PowerSource { // 자동차 구동력을 표현. 엔진 또는 전기모터 등.
+	String drivingpower(String fueltype);
 }
 
 interface Feature { // 자동차의 활용에 대한 정보 표현. 승용, 운반용, 적재용량, 짐칸 크기 등
-	
+	String howuse(String cartype);
 }
--- 실습문제/자동차 정보 출력 실습/Vehicle.java
+++ 실습문제/자동차 정보 출력 실습/Vehicle.java
@@ -14,17 +14,18 @@ public abstract class Vehicle {
 
 	@Override
 	public String toString() {
-		return String.format("[%d년식 (%s)]", vehicle_birth, brand);
+		return String.format("[%d년식 (%s)] ", vehicle_birth, brand);
 	}
 
 }
 
-class Sedan extends Vehicle {
+class Sedan extends Vehicle  {
 	int maxspeed;
 	int drive_per_fuel;
 
 	public Sedan(String brand, int vehicle_birth, int maxspeed, int drive_per_fuel) {
 		super(brand, vehicle_birth);
+		this.maxspeed = maxspeed;
 		this.drive_per_fuel = drive_per_fuel;
 	}
 
@@ -43,16 +44,18 @@ class Sedan extends Vehicle {
 		return false;
 	}
 
-	public String isgoodfuel(int drive_per_fuel) {
+	public String fuel_efficiency(int drive_per_fuel) {
 		if (drive_per_fuel > 10)
 			return "Good";
 		return "Bad";
 	}
+	
+	
 
 	@Override
 	public String toString() {
 		return String.format("%s 최고속도: %dkm, 연비: %dkm, 연비상태: %s", super.toString(), maxspeed, drive_per_fuel,
-				isgoodfuel(drive_per_fuel));
+				fuel_efficiency(drive_per_fuel));
 	}
 
 }
@@ -88,7 +91,7 @@ class Notchback extends Sedan {
 
 	@Override
 	public String toString() {
-		return String.format("%s, 문 개수: %d, 문의 개수가 많은가? : %s ,트렁크의 위치: %s, 뒷자석 에어백이 작동유무: %b, 자율주행모드 여부: %b ",
+		return String.format("%s, 문 개수: %d, 문의 개수가 많은가? : %s, 트렁크의 위치: %s, 뒷자석 에어백이 작동유무: %b, 자율주행모드 여부: %b",
 				super.toString(), door_count, doorcountisbig(door_count), trunk_position,
 				operate_airbag_backseat(vehicle_birth), full_automation_drivemode(brand));
 	}
@@ -96,58 +99,151 @@ class Notchback extends Sedan {
 }
 
 class Hatchback extends Sedan {
-	public Hatchback(String brand, int vehicle_birth, int maxspeed, int drive_per_fuel) {
-		super(brand, vehicle_birth, maxspeed, drive_per_fuel);
-		// TODO Auto-generated constructor stub
-	}
 
+	int trunksize;
 	int count_trunk_in_golfbag;
-	String door_open;
-}
 
-class SUV extends Vehicle {
-	public SUV(String brand, int vehicle_birth) {
-		super(brand, vehicle_birth);
+	public Hatchback(String brand, int vehicle_birth, int maxspeed, int drive_per_fuel, int trunksize,
+			int count_trunk_in_golfbag) {
+		super(brand, vehicle_birth, maxspeed, drive_per_fuel);
+		this.trunksize = trunksize;
+		this.count_trunk_in_golfbag = count_trunk_in_golfbag;
 	}
-
-	int drive_per_fuel;
-	int humanoccupancy;
 
 	@Override
 	boolean operate_airbag_backseat(int vehicle_birth) {
+		return super.operate_airbag_backseat(vehicle_birth);
+	}
 
+	@Override
+	boolean full_automation_drivemode(String brand) {
+		return super.full_automation_drivemode(brand);
+	}
+
+	public String trunk_is_big_or_small(int trunkszie) {
+		if (trunksize > 3) {
+			return "크다";
+		}
+		return "작다";
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s, 트렁크 크기: %s, 트렁크에 들어가는 골프백 최대개수: %d, 뒷자석 에어백이 작동유무: %b, 자율주행모드 여부: %b",
+				super.toString(), trunk_is_big_or_small(trunksize), count_trunk_in_golfbag,
+				operate_airbag_backseat(vehicle_birth), full_automation_drivemode(brand));
+	}
+
+}
+
+class SUV extends Vehicle {
+
+	int drive_per_fuel;
+	int humannum;
+
+	public SUV(String brand, int vehicle_birth, int drive_per_fuel, int humannum) {
+		super(brand, vehicle_birth);
+		this.drive_per_fuel = drive_per_fuel;
+		this.humannum = humannum;
+	}
+
+	@Override
+	boolean operate_airbag_backseat(int vehicle_birth) {
+		if (vehicle_birth > 2000)
+			return true;
 		return false;
 	}
 
 	@Override
 	boolean full_automation_drivemode(String brand) {
+		if (brand.equals("테슬라") || brand.equals("벤츠") || brand.equals("포르쉐") || brand.equals("람보르기니")
+				|| brand.equals("페라리"))
+			return true;
+		return false;
+	}
 
-		return true;
+	public String limithuman(int human) {
+		if (human > 6) {
+			return "최대 수용인원 초과";
+		}
+		return "최대 수용인원 미달";
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s 연비: %dkm, 탑승인원: %d명 %s ", super.toString(), drive_per_fuel, humannum,
+				limithuman(humannum));
 	}
 
 }
 
 class Truck extends SUV {
-	public Truck(String brand, int vehicle_birth) {
-		super(brand, vehicle_birth);
-		// TODO Auto-generated constructor stub
-	}
 
-	int intake_weight;
+	int now_weight;
 	String cargotype;
 
+	public Truck(String brand, int vehicle_birth, int drive_per_fuel, int humannum, int now_weight, String cargotype) {
+		super(brand, vehicle_birth, drive_per_fuel, humannum);
+		this.now_weight = now_weight;
+		this.cargotype = cargotype;
+	}
+
+	@Override
+	boolean operate_airbag_backseat(int vehicle_birth) {
+		return super.operate_airbag_backseat(vehicle_birth);
+	}
+
+	@Override
+	boolean full_automation_drivemode(String brand) {
+		return super.full_automation_drivemode(brand);
+	}
+
 	public boolean takeOff_cargo(String cargotype) {
+		if (cargotype.equals("화학") || cargotype.equals("방사능") || cargotype.equals("오염물질") || cargotype.equals("가스"))
+			return true;
 		return false;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s 현재 중량: %d, 화물 종류: %s, 화물 종류에 따른 화물 분리가능여부: %b, 뒷자석 에어백이 작동유무: %b, 자율주행모드 여부: %b ",
+				super.toString(), now_weight, cargotype, takeOff_cargo(cargotype),
+				operate_airbag_backseat(vehicle_birth), full_automation_drivemode(brand));
 	}
 }
 
 class CrossoOver extends SUV {
-	public CrossoOver(String brand, int vehicle_birth) {
-		super(brand, vehicle_birth);
-		// TODO Auto-generated constructor stub
-	}
 
 	String roadtype;
-	boolean withfamily;
+	int carcost;
+
+	public CrossoOver(String brand, int vehicle_birth, int drive_per_fuel, int humannum, String roadtype, int carcost) {
+		super(brand, vehicle_birth, drive_per_fuel, humannum);
+		this.roadtype = roadtype;
+		this.carcost = carcost;
+
+	}
+
+	@Override
+	boolean operate_airbag_backseat(int vehicle_birth) {
+		// TODO Auto-generated method stub
+		return super.operate_airbag_backseat(vehicle_birth);
+	}
+
+	@Override
+	boolean full_automation_drivemode(String brand) {
+		// TODO Auto-generated method stub
+		return super.full_automation_drivemode(brand);
+	}
+
+	public int repairpay(int carcost) {
+		return carcost / 10;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s 자동차에 적합한 도로: %s, 자동차 수리비: %d, 뒷자석 에어백이 작동유무: %b, 자율주행모드 여부: %b", super.toString(),
+				roadtype, repairpay(carcost), operate_airbag_backseat(vehicle_birth), full_automation_drivemode(brand));
+	}
 
 }
